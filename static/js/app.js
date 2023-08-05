361a2edc584cfdcc15aca601f3553de6ea418f84
eric-blanchard <127366021+eric-blanchard@users.noreply.github.com>
2023-08-04 21:57:12 -0700

    simplify

--- static/js/app.js
+++ static/js/app.js
@@ -68,7 +68,6 @@ function buildCharts(sample) {
 
 function init() {
     let selector = d3.select("#selDataset");
-    // d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json").then((data) => {
     d3.json("./samples.json").then((data) => {
         let sampleNames = data.names;
   
