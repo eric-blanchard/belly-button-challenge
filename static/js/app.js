f30dcd4c3685b5d3200f2fc3c0100adae2cbe72d
eric-blanchard <127366021+eric-blanchard@users.noreply.github.com>
2023-08-04 21:54:12 -0700

    trying something

--- static/js/app.js
+++ static/js/app.js
@@ -68,8 +68,9 @@ function buildCharts(sample) {
 
 function init() {
     let selector = d3.select("#selDataset");
-    d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json").then((data) => {
-      let sampleNames = data.names;
+    // d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json").then((data) => {
+    d3.json("./samples.json").then((data) => {
+        let sampleNames = data.names;
   
       for (let i = 0; i < sampleNames.length; i++){
         selector.append("option").text(sampleNames[i]).property("value", sampleNames[i]);
